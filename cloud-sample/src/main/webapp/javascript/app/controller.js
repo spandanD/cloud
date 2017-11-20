7c1dc4369099566a4c6907750225e437e4f42588
Spandan <Spandan@LAPTOP-RR6C8RK0>
2017-11-20 13:12:21 -0500

    Controller file adding responses to text area

--- cloud-sample/src/main/webapp/javascript/app/controller.js
+++ cloud-sample/src/main/webapp/javascript/app/controller.js
@@ -1,23 +1,42 @@
 function appController(){
 	var config = new Config();
 	var DIV_ID = "message-content";
+	var request = {url: config.EVENT_URL , 
+			       successCallback: render};
+	
+	var cloudResource = new Cloud(request);
 	
 	return{
 		launch: launch
 	}
 	
 	function launch(){
-		$.get(config.EVENT_URL, {}, render);
+		cloudResource.subscribe();
+		keyListenerOnTextArea();
 	}
 	
 	function render(response){
 		var placeholder = $('#' + DIV_ID);
-		placeholder.html(response);
+		placeholder.append("<br/>")
+		placeholder.append(response);
+	}
+	
+	function keyListenerOnTextArea(){
+		var that = $('#text_area');
+		that.keypress(function(e){
+			if (e.which == 13 && !e.shiftKey){
+				e.preventDefault();
+				cloudResource.publish(that.val());
+				that.val("");
+			}
+				
+		});
 	}
 }
 
 $(document).ready(function(){
-	appController().launch()
+	appController().launch();
+	
 });
 
 
