723b37798390896714b9a06fb48227014aca998b
Spandan <Spandan@LAPTOP-RR6C8RK0>
2017-11-22 12:57:36 -0500

    channel broadcaster to suspended connections using thread pool

---
+++ cloud-core/src/main/java/org/sdas/cloud/core/runnable/ChannelBroadcasterRunnable.java
@@ -0,0 +1,36 @@
+package org.sdas.cloud.core.runnable;
+
+import javax.servlet.AsyncContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.sdas.cloud.core.entity.Channel;
+import org.sdas.cloud.core.entity.ChannelStreamer;
+
+public class ChannelBroadcasterRunnable implements Runnable{
+	
+	private final static Logger logger = LogManager.getLogger(ChannelBroadcasterRunnable.class);
+	private ChannelStreamer<?> channelStreamer;
+	
+	public ChannelBroadcasterRunnable(ChannelStreamer<?> channelStreamer){
+		this.channelStreamer = channelStreamer;
+	}
+	
+	public void run() {
+		AsyncContext asyncContext = channelStreamer.getAsyncContext();
+		Channel<?> channel = channelStreamer.getChannel();
+		Object content = channel.getContent();
+		logger.info("Broadcasting {} using thread: {}" , content, Thread.currentThread().getName());		
+		try{
+			asyncContext.getResponse().getWriter().println(content);
+			asyncContext.complete();
+			logger.info("Broadcast to suspended connection complete");
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+			//TODO try to broadcast later?
+			logger.error("Could not broadcast message to connection due to:" + ex.getMessage());
+		}
+	}
+
+}
